b9ccf8977535c2931aa5079a65112313cc14dab6
JannisNe <necker@physik.hu-berlin.de>
2022-01-20 17:38:33 +0100

    setup file

---
+++ setup.py
@@ -0,0 +1,40 @@
+import setuptools
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+if __name__ == '__main__':
+    setuptools.setup(
+        name="timewise",
+        version="0.1",
+        author="Jannis Necker",
+        author_email="jannis.necker@gmail.com",
+        description="A small package to download infrared data from the WISE satellite",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        license="MIT",
+        keywords="WISE data infrared astronomy",
+        url="https://github.com/JannisNe/timewise",
+        project_urls={
+            "Bug Tracker": "https://github.com/JannisNe/timewise/issues",
+        },
+        packages=setuptools.find_packages(),
+        classifiers=[
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+            "Programming Language :: Python :: 3.9",
+        ],
+        python_requires='>=3.9',
+        install_requires=[
+            "tqdm==4.62.3",
+            "requests==2.27.1",
+            "pandas==1.3.5",
+            "numpy==1.22.1",
+            "pyvo==1.1",
+            "astropy==5.0",
+            "matplotlib==3.5.1",
+            "coveralls==3.3.1",
+            "scikit-image==0.19.1",
+        ]
+    )
