9c7f231406c429290f6730f4190364ea2cbe64a2
JannisNe <necker@physik.hu-berlin.de>
2022-01-24 10:49:51 +0100

    remove pyfon 10 bc pyvo not compatible

--- setup.py
+++ setup.py
@@ -25,7 +25,6 @@ if __name__ == '__main__':
             "Operating System :: OS Independent",
             "Programming Language :: Python :: 3.8",
             "Programming Language :: Python :: 3.9",
-            "Programming Language :: Python :: 3.10"
         ],
         python_requires='>=3.9',
         install_requires=[
