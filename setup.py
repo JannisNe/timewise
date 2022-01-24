3d072dacc47a95810dd42b336ef1fd432cf981ae
JannisNe <necker@physik.hu-berlin.de>
2022-01-24 11:11:21 +0100

    fix version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 if __name__ == '__main__':
     setuptools.setup(
         name="timewise",
-        version="0.1.3test2",
+        version="0.1.3.dev2",
         author="Jannis Necker",
         author_email="jannis.necker@gmail.com",
         description="A small package to download infrared data from the WISE satellite",
