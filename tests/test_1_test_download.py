546f7cdec08f0228fe3b263953f23a94274e995f
JannisNe <necker@physik.hu-berlin.de>
2022-11-05 10:22:27 +0100

    load from bigdata dir

--- tests/test_1_test_download.py
+++ tests/test_1_test_download.py
@@ -158,7 +158,7 @@ class TestMIRFlareCatalogue(unittest.TestCase):
             )
 
             N_downloaded = sum([
-                len(wise_desy_bigdata._load_data_product(service="tap", chunk_number=c))
+                len(wise_desy_bigdata._load_data_product(service="tap", chunk_number=c, load_from_bigdata_dir=True))
                 for c in range(wise_desy_bigdata.n_chunks)
             ])
 
