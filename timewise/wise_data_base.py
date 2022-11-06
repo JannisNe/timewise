b738f987ec9d4a215f9cc5891b995a5af648e266
JannisNe <necker@physik.hu-berlin.de>
2022-11-06 12:49:40 +0100

    bring back save and load lightcurves

--- timewise/wise_data_base.py
+++ timewise/wise_data_base.py
@@ -571,6 +571,49 @@ class WISEDataBase(abc.ABC):
 
         self._combine_data_products(service=service, remove=remove_chunks, overwrite=overwrite)
 
+    def _lightcurve_filename(self, service, chunk_number=None, jobID=None):
+
+        warnings.warn("Separate lightcurves and metadata will be deprecated in v0.3.0!", DeprecationWarning)
+
+        if (chunk_number is None) and (jobID is None):
+            return os.path.join(self.lightcurve_dir, f"binned_lightcurves_{service}.json")
+        else:
+            fn = f"binned_lightcurves_{service}{self._split_chunk_key}{chunk_number}"
+            if (chunk_number is not None) and (jobID is None):
+                return os.path.join(self._cache_photometry_dir, fn + ".json")
+            else:
+                return os.path.join(self._cache_photometry_dir, fn + f"_{jobID}.json")
+
+    def _load_lightcurves(self, service, chunk_number=None, jobID=None, return_filename=False):
+        fn = self._lightcurve_filename(service, chunk_number, jobID)
+        logger.debug(f"loading {fn}")
+        try:
+            with open(fn, "r") as f:
+                lcs = json.load(f)
+            if return_filename:
+                return lcs, fn
+            return lcs
+        except FileNotFoundError:
+            logger.warning(f"No file {fn}")
+
+    def _save_lightcurves(self, lcs, service, chunk_number=None, jobID=None, overwrite=False):
+        fn = self._lightcurve_filename(service, chunk_number, jobID)
+        logger.debug(f"saving {len(lcs)} new lightcurves to {fn}")
+
+        if fn == self._lightcurve_filename(service):
+            self._cached_final_products['lightcurves'][service] = lcs
+
+        if not overwrite:
+            try:
+                old_lcs = self._load_lightcurves(service=service, chunk_number=chunk_number, jobID=jobID)
+                logger.debug(f"Found {len(old_lcs)}. Combining")
+                lcs = lcs.update(old_lcs)
+            except FileNotFoundError as e:
+                logger.info(f"FileNotFoundError: {e}. Making new binned lightcurves.")
+
+        with open(fn, "w") as f:
+            json.dump(lcs, f)
+
     def _data_product_filename(self, service, chunk_number=None, jobID=None):
 
         n = "timewise_data_product_"
