ec68f11ed921f88ebfd42c86bdc06836388a9f41
JannisNe <necker@physik.hu-berlin.de>
2023-03-07 17:55:52 +0100

    add version

--- timewise/__init__.py
+++ timewise/__init__.py
@@ -1,3 +1,5 @@
 from timewise.wise_data_by_visit import WiseDataByVisit
 from timewise.wise_bigdata_desy_cluster import WISEDataDESYCluster
 from timewise.parent_sample_base import ParentSampleBase
+
+__version__ = "0.3.6"
