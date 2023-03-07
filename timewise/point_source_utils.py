1f60b273eefbb403bfd8bf743f5f3eea0792bf3d
JannisNe <necker@physik.hu-berlin.de>
2023-03-07 17:55:18 +0100

    remove redundant imports

--- timewise/point_source_utils.py
+++ timewise/point_source_utils.py
@@ -1,10 +1,8 @@
 import logging
 import pandas as pd
-import matplotlib.pyplot as plt
 
 from timewise.parent_sample_base import ParentSampleBase
 from timewise.wise_data_by_visit import WiseDataByVisit
-from timewise.utils import plot_sdss_cutout
 
 
 logger = logging.getLogger(__name__)
@@ -29,24 +27,7 @@ def get_point_source_parent_sample(base_name, ra, dec):
             super().__init__(base_name=base_name)
 
             self.base_name = base_name
-            self.df = pd.DataFrame({'ra': [ra], 'dec': [dec], 'id': ['NGC 1068']})
-
-        def _plot_cutout(self, ra, dec, arcsec, interactive, title=None, fn=None, save=False, ax=None, **kwargs):
-            h = kwargs.get('height', 2)
-            if not ax:
-                fig, ax = plt.subplots(figsize=(h, h), sharex='all')
-            else:
-                fig = plt.gcf()
-            plot_sdss_cutout(ra, dec, interactive=True, ax=ax, arcsec=arcsec, title='SDSS', **kwargs)
-
-            if interactive:
-                return fig, ax
-            if save:
-                fig.savefig(fn)
-                plt.close()
-
-            plt.show()
-            plt.close()
+            self.df = pd.DataFrame({'ra': [ra], 'dec': [dec], 'id': [base_name]})
 
         def save_local(self):
             logger.debug(f"not saving")
