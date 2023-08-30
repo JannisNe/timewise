4d823fe06fa2b09e49d4d71d0d271c555b4e33f7
JannisNe <necker@physik.hu-berlin.de>
2023-08-30 11:11:02 +0200

    dont do something special when on rtd server

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,6 +11,7 @@
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
 import os
+import sphinx_rtd_theme
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
@@ -54,12 +55,8 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 # Readthedocs theme
-# on_rtd is whether on readthedocs.org, this line of code grabbed from docs.readthedocs.org...
-on_rtd = os.environ.get("READTHEDOCS", None) == "True"
-if not on_rtd:  # only import and set the theme if we're building docs locally
-    import sphinx_rtd_theme
-    html_theme = "sphinx_rtd_theme"
-    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_theme = "sphinx_rtd_theme"
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 html_css_files = ["readthedocs-custom.css"] # Override some CSS settings
 
 # Add any paths that contain custom static files (such as style sheets) here,
