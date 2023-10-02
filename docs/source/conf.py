82983b942a6ba3d2a880c008d8ba115427986436
JannisNe <necker@physik.hu-berlin.de>
2023-10-02 10:14:16 +0200

    bump version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = '2022, Jannis Necker'
 author = 'Jannis Necker'
 
 # The full version, including alpha/beta/rc tags
-release = 'v0.4.5'
+release = 'v0.4.6'
 
 
 # -- General configuration ---------------------------------------------------
