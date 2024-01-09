009b759459be37fb45004451a898fd7f4756d8a9
JannisNe <necker@physik.hu-berlin.de>
2024-01-09 10:17:58 +0100

    bump version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = '2022, Jannis Necker'
 author = 'Jannis Necker'
 
 # The full version, including alpha/beta/rc tags
-release = 'v0.4.9'
+release = 'v0.4.10'
 
 
 # -- General configuration ---------------------------------------------------
--- timewise/__init__.py
+++ timewise/__init__.py
@@ -2,4 +2,4 @@ from timewise.wise_data_by_visit import WiseDataByVisit
 from timewise.wise_bigdata_desy_cluster import WISEDataDESYCluster
 from timewise.parent_sample_base import ParentSampleBase
 
-__version__ = "0.4.9"
+__version__ = "0.4.10"
