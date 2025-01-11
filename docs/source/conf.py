77858725886315660d8afa96fe572a9664d35c9b
Jannis Necker <necker@physik.hu-berlin.de>
2025-01-11 19:21:43 +0100

    Extend markers (#140)
    * more markers for long wise lightcurves
    
    * bump version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = '2022, Jannis Necker'
 author = 'Jannis Necker'
 
 # The full version, including alpha/beta/rc tags
-release = 'v0.4.11'
+release = 'v0.4.12'
 
 
 # -- General configuration ---------------------------------------------------
