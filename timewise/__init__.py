e655c663430b6e3fd40aeb0254bfd5e768974285
JannisNe <necker@physik.hu-berlin.de>
2025-09-05 10:34:41 +0200

    use importlib to dynamically determine package version

--- timewise/__init__.py
+++ timewise/__init__.py
@@ -1,5 +1,7 @@
+from importlib.metadata import version
+
 from timewise.wise_data_by_visit import WiseDataByVisit
 from timewise.wise_bigdata_desy_cluster import WISEDataDESYCluster
 from timewise.parent_sample_base import ParentSampleBase
 
-__version__ = "0.4.12"
+__version__ = version("timewise")
