8dfa5909dbc838ffe3e934128bf36639dbd4e812
JannisNe <necker@physik.hu-berlin.de>
2025-09-09 13:22:40 +0200

    raise requests.HTTPError from TAPQuery.submit() response to avoid silent fails

--- timewise/utils.py
+++ timewise/utils.py
@@ -431,8 +431,47 @@ def get_excess_variance(y, y_err, mu):
 
 class StableAsyncTAPJob(vo.dal.AsyncTAPJob):
     """
-    Implements backoff for call of phase which otherwise breaks the code if there are connection issues
+    Implements backoff for call of phase which otherwise breaks the code if there are connection issues.
+    Also stores the response of TapQuery.submit() under self.submit_response for debugging
     """
+
+    def __init__(self, url, *, session=None, delete=True):
+        super(StableAsyncTAPJob, self).__init__(url, session=session, delete=delete)
+        self.submit_response = None
+
+    @classmethod
+    def create(
+            cls, baseurl, query, *, language="ADQL", maxrec=None, uploads=None,
+            session=None, **keywords):
+        """
+        creates a async tap job on the server under ``baseurl``
+        Raises requests.HTTPError if TAPQuery.submit() failes.
+
+        Parameters
+        ----------
+        baseurl : str
+            the TAP baseurl
+        query : str
+            the query string
+        language : str
+            specifies the query language, default ADQL.
+            useful for services which allow to use the backend query language.
+        maxrec : int
+            the maximum records to return. defaults to the service default
+        uploads : dict
+            a mapping from table names to objects containing a votable
+        session : object
+           optional session to use for network requests
+        """
+        tapquery = vo.dal.TAPQuery(
+            baseurl, query, mode="async", language=language, maxrec=maxrec,
+            uploads=uploads, session=session, **keywords)
+        response = tapquery.submit()
+        response.raise_for_status()
+        job = cls(response.url, session=session)
+        job._client_set_maxrec = maxrec
+        job.submit_response = response
+        return job
 
     @property
     @backoff.on_exception(
