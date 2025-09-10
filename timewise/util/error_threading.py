e3db02694b4f7897a3720c730f8ad307f6d0d6a6
JannisNe <necker@physik.hu-berlin.de>
2025-09-10 15:39:26 +0200

    add stop event to queue, explicit argument parsing in thread subclass

--- timewise/util/error_threading.py
+++ timewise/util/error_threading.py
@@ -1,14 +1,16 @@
 import sys
 from queue import Queue
-from threading import Thread
+from threading import Thread, Event
+from typing import Any, Optional, Callable, Mapping
 
 
 class ErrorQueue(Queue):
     """Queue subclass whose join() re-raises exceptions from worker threads."""
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, stop_event: Event, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.error_queue = Queue()
+        self.stop_event = stop_event
 
     def report_error(self, exc_info):
         """Called by workers to push an exception into the error queue."""
@@ -24,6 +26,7 @@ class ErrorQueue(Queue):
             while self.unfinished_tasks:
                 if not self.error_queue.empty():
                     exc_info = self.error_queue.get()
+                    self.stop_event.set()
                     raise exc_info[1].with_traceback(exc_info[2])
                 self.all_tasks_done.wait()
 
@@ -31,8 +34,18 @@ class ErrorQueue(Queue):
 class ExceptionSafeThread(Thread):
     """Thread subclass that reports uncaught exceptions to the ErrorQueue."""
 
-    def __init__(self, error_queue: ErrorQueue, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(
+        self,
+        error_queue: Any,
+        *,
+        group: Optional[Any] = None,
+        target: Optional[Callable[..., Any]] = None,
+        name: Optional[str] = None,
+        args: tuple = (),
+        kwargs: Optional[Mapping[str, Any]] = None,
+        daemon: Optional[bool] = None
+    ):
+        super().__init__(group=group, target=target, name=name, args=args, kwargs=kwargs, daemon=daemon)
         self.error_queue = error_queue
 
     def run(self):
