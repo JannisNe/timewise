67431292717131b0e4f19cab2e4227d74bdb8ec6
JannisNe <necker@physik.hu-berlin.de>
2025-09-15 21:03:20 +0200

    remove reduntant query instances

--- timewise/query/__init__.py
+++ timewise/query/__init__.py
@@ -1,6 +1,6 @@
 from pydantic import Field
 from typing import Union, Annotated, TypeAlias
-from .positional.base import PositionalQuery
+from .positional import PositionalQuery
 
 # Discriminated union of all query types
 QueryType: TypeAlias = Annotated[Union[PositionalQuery], Field(discriminator="type")]
--- timewise/query/positional.py
+++ timewise/query/positional.py
@@ -1,7 +1,7 @@
 import logging
 from typing import Literal
 
-from ..base import Query
+from .base import Query
 
 logger = logging.getLogger(__name__)
 
