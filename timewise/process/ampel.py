0cfa2913f4e63504fd267854278bd4e90db4cd03
JannisNe <necker@physik.hu-berlin.de>
2025-09-24 13:10:21 +0200

    add makeing ampel job file from timewise conf

---
+++ timewise/process/ampel.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+import logging
+
+from ..config import TimewiseConfig
+
+
+logger = logging.getLogger(__name__)
+DEFAULT_TEMPALTE_PATH = Path(__file__).parent / "template.yml"
+
+
+def make_ampel_job_file(cfg_path: Path, template_path: Path | None = None) -> Path:
+    cfg = TimewiseConfig.from_yaml(cfg_path)
+    template_path = template_path or DEFAULT_TEMPALTE_PATH
+    logger.debug(f"loading ampel job template from {template_path}")
+    with template_path.open("r") as f:
+        template = f.read()
+
+    # the timewise config makes sure there is only one original id key
+    ori_id_key = cfg.download.queries[0].original_id_key
+
+    ampel_job = template.replace("TIMEWISE_CONFIG_PATH", str(cfg_path)).replace(
+        "ORIGINAL_ID_KEY", ori_id_key
+    )
+
+    ampel_job_path = cfg_path.parent / f"{cfg_path.stem}_ampel_job.yml"
+    logger.info(f"writing ampel job to {ampel_job_path}")
+    with ampel_job_path.open("w") as f:
+        f.write(ampel_job)
+
+    return ampel_job_path
