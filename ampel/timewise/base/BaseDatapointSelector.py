83fb45b2a408bc411d78d32a757d00201aecf4a7
JannisNe <necker@physik.hu-berlin.de>
2025-09-25 17:07:56 +0200

    try and propagate logger

--- ampel/timewise/base/BaseDatapointSelector.py
+++ ampel/timewise/base/BaseDatapointSelector.py
@@ -15,9 +15,30 @@ from ampel.types import DataPointId
 from ampel.base.AmpelBaseModel import AmpelBaseModel
 from ampel.base.AmpelABC import AmpelABC
 from ampel.base.decorator import abstractmethod
+from ampel.log.AmpelLogger import AmpelLogger
+from ampel.struct.Resource import Resource
 
 
 class BaseDatapointSelector(AmpelBaseModel, AmpelABC, abstract=True):
+    @property
+    def logger(self) -> AmpelLogger:
+        return self._logger
+
+    @property
+    def resources(self) -> dict[str, Resource]:
+        return self._resources
+
+    def __init__(self, **kwargs) -> None:
+        super().__init__(**kwargs)
+        self._logger: AmpelLogger = AmpelLogger.get_logger()
+        self._resources: dict[str, Resource] = {}
+
+    def set_logger(self, logger: AmpelLogger) -> None:
+        self._logger = logger
+
+    def add_resource(self, name: str, value: Resource) -> None:
+        self._resources[name] = value
+
     @abstractmethod
     def select(
         self, datapoints: Iterable[DataPoint]
