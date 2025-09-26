06ab0b2cdc3a9f5cf6b6dc193868ba76c3843e4d
JannisNe <necker@physik.hu-berlin.de>
2025-09-26 10:50:54 +0200

    remove redundant bits

--- ampel/timewise/t1/T1StackVisits.py
+++ ampel/timewise/t1/T1StackVisits.py
@@ -8,23 +8,14 @@
 # Last Modified By:    Jannis Necker <jannis.necker@gmail.com>
 
 from ampel.abstract.AbsT1ComputeUnit import AbsT1ComputeUnit
-from ampel.base.AuxUnitRegister import AuxUnitRegister
 from ampel.content.DataPoint import DataPoint
 from ampel.struct.UnitResult import UnitResult
-from ampel.model.UnitModel import UnitModel
 from ampel.types import StockId, UBson
-
-from ampel.timewise.base.BaseDatapointSelector import BaseDatapointSelector
 
 
 class T1StackVisits(AbsT1ComputeUnit):
-    select: UnitModel
-
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self._combine: BaseDatapointSelector = AuxUnitRegister.new_unit(
-            model=self.select, sub_type=BaseDatapointSelector
-        )
 
     def compute(
         self, datapoints: list[DataPoint]
