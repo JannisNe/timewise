9162cb9ea77064151185a71134d7385b3dc51667
JannisNe <necker@physik.hu-berlin.de>
2025-09-27 15:45:35 +0200

    move backof handler to separate module

--- timewise/io/stable_tap.py
+++ timewise/io/stable_tap.py
@@ -3,16 +3,10 @@ import backoff
 import pyvo as vo
 from xml.etree import ElementTree
 
+from timewise.util.backoff import backoff_hndlr
+
 
 logger = logging.getLogger(__name__)
-
-
-def backoff_hndlr(details):
-    logger.info(
-        "Backing off {wait:0.1f} seconds after {tries} tries "
-        "calling function {target} with args {args} and kwargs "
-        "{kwargs}".format(**details)
-    )
 
 
 class StableAsyncTAPJob(vo.dal.AsyncTAPJob):
