f5c7c3394b41b27024ce0251e2c8abc6ed5e7619
JannisNe <necker@physik.hu-berlin.de>
2025-09-27 15:41:19 +0200

    add a common lightcurve plotting function

---
+++ timewise/plot/lightcurve.py
@@ -0,0 +1,100 @@
+from typing import Dict
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+from timewise.process import keys
+
+
+BAND_PLOT_COLORS = {"W1": "r", "W2": "b"}
+
+
+def plot_lightcurve(
+    lum_key: str,
+    stacked_lightcurve: pd.DataFrame | None = None,
+    raw_lightcurve: pd.DataFrame | None = None,
+    ax: plt.Axes | None = None,
+    colors: Dict[str, str] | None = None,
+    **kwargs,
+) -> tuple[plt.Figure, plt.Axes]:
+    assert stacked_lightcurve or raw_lightcurve
+
+    if not colors:
+        colors = BAND_PLOT_COLORS
+
+    if not ax:
+        fig, ax = plt.subplots(**kwargs)
+    else:
+        fig = plt.gcf()
+
+    for b in ["W1", "W2"]:
+        try:
+            if isinstance(stacked_lightcurve, pd.DataFrame):
+                ul_mask = np.array(
+                    stacked_lightcurve[f"{b}_{lum_key}{keys.UPPER_LIMIT}"]
+                ).astype(bool)
+                ax.errorbar(
+                    stacked_lightcurve[keys.MEAN + "_mjd"][~ul_mask],
+                    stacked_lightcurve[f"{b}{keys.MEAN}_{lum_key}"][~ul_mask],
+                    yerr=stacked_lightcurve[f"{b}_{lum_key}{keys.RMS}"][~ul_mask],
+                    label=b,
+                    ls="",
+                    marker="s",
+                    c=colors[b],
+                    markersize=4,
+                    markeredgecolor="k",
+                    ecolor="k",
+                    capsize=2,
+                )
+                ax.scatter(
+                    stacked_lightcurve[keys.MEAN + "_mjd"][ul_mask],
+                    stacked_lightcurve[f"{b}{keys.MEAN}_{lum_key}"][ul_mask],
+                    marker="v",
+                    c=colors[b],
+                    alpha=0.7,
+                    s=2,
+                )
+
+            if isinstance(raw_lightcurve, pd.DataFrame):
+                m = ~raw_lightcurve[f"{b}_{lum_key}"].isna()
+                ul_mask = raw_lightcurve[f"{b}_{lum_key}{keys.ERROR_EXT}"].isna()
+
+                tot_m = m & ~ul_mask
+                if np.any(tot_m):
+                    ax.errorbar(
+                        raw_lightcurve.mjd[tot_m],
+                        raw_lightcurve[f"{b}_{lum_key}"][tot_m],
+                        yerr=raw_lightcurve[f"{b}_{lum_key}{keys.ERROR_EXT}"][tot_m],
+                        label=f"{b} unbinned",
+                        ls="",
+                        marker="o",
+                        c=colors[b],
+                        markersize=4,
+                        alpha=0.3,
+                    )
+
+                single_ul_m = m & ul_mask
+                if np.any(single_ul_m):
+                    label = f"{b} unbinned upper limits" if not np.any(tot_m) else ""
+                    ax.scatter(
+                        raw_lightcurve.mjd[single_ul_m],
+                        raw_lightcurve[f"{b}_{lum_key}"][single_ul_m],
+                        marker="d",
+                        c=colors[b],
+                        alpha=0.3,
+                        s=1,
+                        label=label,
+                    )
+
+        except KeyError as e:
+            raise KeyError(f"Could not find brightness key {e}!")
+
+    if lum_key == "mag":
+        ylim = ax.get_ylim()
+        ax.set_ylim(max(ylim), min(ylim))
+
+    ax.set_xlabel("MJD")
+    ax.set_ylabel(lum_key)
+    ax.legend()
+
+    return fig, ax
