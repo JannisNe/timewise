7eb2dff5983d845070f12281f44b273b19bc24a1
JannisNe <necker@physik.hu-berlin.de>
2025-09-27 16:51:46 +0200

    move visit map to separate module

--- ampel/timewise/t1/T1StackVisits.py
+++ ampel/timewise/t1/T1StackVisits.py
@@ -21,6 +21,7 @@ from ampel.types import StockId, UBson
 from ampel.timewise.util.pdutil import datapoints_to_dataframe
 
 from timewise.process import keys
+from timewise.util.visits import get_visit_map
 
 
 class T1StackVisits(AbsT1ComputeUnit):
@@ -254,7 +255,7 @@ class T1StackVisits(AbsT1ComputeUnit):
         """
 
         # -------------------------   create visit mask   -------------------------- #
-        visit_map = self.get_visit_map(lightcurve)
+        visit_map = get_visit_map(lightcurve)
         counts = np.bincount(visit_map)
 
         stacked_data = dict()
