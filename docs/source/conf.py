907b93e4f81e84bffd83566693660cee2da79693
JannisNe <necker@physik.hu-berlin.de>
2025-09-30 12:40:41 +0200

    bump version indicators

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,12 +12,12 @@ import sphinx_rtd_theme
 
 # -- Project information -----------------------------------------------------
 
-project = 'timewise'
-copyright = '2022, Jannis Necker'
-author = 'Jannis Necker'
+project = "timewise"
+copyright = "2022, Jannis Necker"
+author = "Jannis Necker"
 
 # The full version, including alpha/beta/rc tags
-release = f'v0.5.3'
+release = "v0.5.4"
 
 
 # -- General configuration ---------------------------------------------------
@@ -26,16 +26,16 @@ release = f'v0.5.3'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.duration',
-    'sphinx.ext.doctest',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.viewcode',
-    "myst_parser"
+    "sphinx.ext.duration",
+    "sphinx.ext.doctest",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.viewcode",
+    "myst_parser",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -51,9 +51,9 @@ exclude_patterns = []
 # Readthedocs theme
 html_theme = "sphinx_rtd_theme"
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-html_css_files = ["readthedocs-custom.css"] # Override some CSS settings
+html_css_files = ["readthedocs-custom.css"]  # Override some CSS settings
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
