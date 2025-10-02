aea5fa6ee759735577ae41392219a251a183cc37
JannisNe <necker@physik.hu-berlin.de>
2025-10-02 11:30:27 +0200

    fix mongorestore

--- tests/test_plot.py
+++ tests/test_plot.py
@@ -11,11 +11,14 @@ def test_make_plot(timewise_config_path, tmp_db_name, tmp_path, cutout):
     print(tmp_db_name)
     cmd = [
         "mongorestore",
-        "--nsFrom='jannisnecker_pytest-14_test_run_ampel1.*'",
-        f"--nsTo='{tmp_db_name}.*'",
+        "--nsFrom=jannisnecker_pytest-14_test_run_ampel1.*",
+        f"--nsTo={tmp_db_name}.*",
         f"--archive={mongoexport_path}",
         "--gzip",
     ]
     subprocess.run(cmd)
 
-    make_plot(timewise_config_path, cutout, [0, 1], tmp_path)
+    indices = [0, 1]
+    make_plot(timewise_config_path, cutout, indices, tmp_path)
+    fns = [tmp_path / f"{index}.pdf" for index in indices]
+    assert all([fn.exists() for fn in fns])
