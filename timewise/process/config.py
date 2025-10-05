608267807f94ace391a1a315607eecddf19cd02a
JannisNe <necker@physik.hu-berlin.de>
2025-10-05 14:58:34 +0200

    make input db name configurable

--- timewise/process/config.py
+++ timewise/process/config.py
@@ -1,7 +1,8 @@
 from pathlib import Path
 import logging
+from typing import Dict, Any
 
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator
 
 from .interface import AmpelInterface
 
@@ -14,10 +15,16 @@ class AmpelConfig(BaseModel):
     mongo_db_name: str
     template_path: Path = DEFAULT_TEMPLATE_PATH
     uri: str = "localhost:27017"
+    input_mongo_db_name: str = ""
 
-    @property
-    def input_mongo_db_name(self) -> str:
-        return self.mongo_db_name + "_input"
+    @field_validator("input_mongo_db_name", mode="after")  # type: ignore
+    @classmethod
+    def default_input_db_name(
+        cls, input_mongo_db_name: str, values: Dict[str, Any]
+    ) -> str:
+        if not input_mongo_db_name:
+            return values["mongo_db_name"] + "_input"
+        return input_mongo_db_name
 
     def build_interface(self, original_id_key: str, input_csv: Path) -> AmpelInterface:
         return AmpelInterface(
