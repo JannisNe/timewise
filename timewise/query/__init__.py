a3fc1182c84c61c5f502e8999d6907e5f685fc5d
Jannis Necker <jannis.necker@gmail.com>
2025-12-02 13:44:00 +0100

    add query by allwise id

--- timewise/query/__init__.py
+++ timewise/query/__init__.py
@@ -1,6 +1,9 @@
+from typing import Annotated, TypeAlias, Union
+
 from pydantic import Field
-from typing import Union, Annotated, TypeAlias
+
+from .by_allwise_cntr import AllWISECntrQuery
 from .positional import PositionalQuery
 
 # Discriminated union of all query types
-QueryType: TypeAlias = Annotated[Union[PositionalQuery], Field(discriminator="type")]
+QueryType: TypeAlias = Annotated[Union[PositionalQuery, AllWISECntrQuery], Field(discriminator="type")]
