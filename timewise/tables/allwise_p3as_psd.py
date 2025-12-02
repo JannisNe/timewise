00767379de82937b6ab1786842fecd076c53a456
Jannis Necker <jannis.necker@gmail.com>
2025-12-02 13:44:20 +0100

    add allwise source catalog

---
+++ timewise/tables/allwise_p3as_psd.py
@@ -0,0 +1,24 @@
+from typing import ClassVar, Dict, Literal, Type
+
+from .base import TableConfig
+
+
+class allwise_p3as_psd(TableConfig):
+    name: Literal["allwise_p3as_psd"] = "allwise_p3as_psd"
+    columns_dtypes: ClassVar[Dict[str, Type]] = {
+        "ra": float,
+        "dec": float,
+        "mjd": float,
+        "cntr": str,
+        "w1mpro": float,
+        "w1sigmpro": float,
+        "w2mpro": float,
+        "w2sigmpro": float,
+        "w1flux": float,
+        "w1sigflux": float,
+        "w2flux": float,
+        "w2sigflux": float,
+    }
+    ra_column: ClassVar[str] = "ra"
+    dec_column: ClassVar[str] = "dec"
+    allwise_column: ClassVar[str] = "cntr"
